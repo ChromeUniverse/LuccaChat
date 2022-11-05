871321ad091d48e00cbd37b3c502a3c40a71fad0
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-05 14:07:08 -0300

    Added optional chat nav for contact/group comp.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import avatar from '../assets/avatar.jpeg'
+import { useChatsStore } from "../zustand/chats-store";
 
 interface Props {
   name: string;
   handle: string;
+  chatId?: string;
 }
 
-function Contact({ name, handle }: Props) {
+function Contact({ name, handle, chatId}: Props) {
+  
+  const setChatId = useChatsStore(state => state.setCurrentChatId);
+
   return (
-    <div className="px-3 py-2 w-full flex items-center gap-3 hover:bg-slate-400 hover:bg-opacity-20 rounded-lg cursor-pointer">
+    <div
+      className="px-3 py-2 w-full flex items-center gap-3 hover:bg-slate-400 hover:bg-opacity-20 rounded-lg cursor-pointer"
+      onClick={() => {
+        if (chatId) setChatId(chatId);
+      }}
+    >
       {/* Avatar */}
-      <div className="w-14 h-14 bg-slate-400 rounded-full"></div>
+      <img className="w-14 h-14 rounded-full" src={avatar} alt="" />
 
       {/* Contact Name */}
       <div className="flex flex-col">
--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,20 +1,33 @@
 import React from "react";
+import creeper from '../assets/creeper.webp'
+import { useChatsStore } from "../zustand/chats-store";
 
 interface Props {
   name: string;
   members: number;
+  chatId?: string;
 }
 
-function Group({ name, members }: Props) {
+function Group({ name, members, chatId }: Props) {
+
+  const setChatId = useChatsStore(state => state.setCurrentChatId);
+
   return (
-    <div className="px-3 py-2 w-full flex items-center gap-3 hover:bg-slate-400 hover:bg-opacity-20 rounded-lg cursor-pointer">
+    <div
+      className="px-3 py-2 w-full flex items-center gap-3 hover:bg-slate-400 hover:bg-opacity-20 rounded-lg cursor-pointer"
+      onClick={() => {
+        if (chatId) setChatId(chatId);
+      }}
+    >
       {/* Avatar */}
-      <div className="w-14 h-14 bg-slate-400 rounded-full"></div>
+      <img className="w-14 h-14 rounded-full" src={creeper} alt="" />
 
       {/* Group Name */}
       <div className="flex flex-col">
         <h3 className="font-normal text-xl">{name}</h3>
-        <p className="font-normal text-sm">{members.toLocaleString()} members</p>
+        <p className="font-normal text-sm">
+          {members.toLocaleString()} members
+        </p>
       </div>
     </div>
   );
