d899a5c99d504d67dee641fdc31790329f1428d2
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-05 14:08:19 -0300

    Chat component overhauld

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Test photo
 import creeper from '../assets/creeper.webp'
+import avatar from '../assets/avatar.jpeg'
 
 // Components
 import Message from './Message'
@@ -11,23 +12,29 @@ import TextareaAutosize from 'react-textarea-autosize';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightFromBracket, faCircleInfo, faEllipsisV, faGear, faPaperPlane, faVolumeXmark, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 
+// Zustand store
+import { useInfoStore } from '../zustand/info-panel-store';
+import { useChatsStore } from '../zustand/chats-store';
+import { ChatType, DMType, GroupType, UserType } from '../data';
+
 
 type MenuLineProps = {
   text: string;
   icon: IconDefinition;
   danger?: boolean;
-  onClick?: (value: 'user' | 'group' | null) => void;
+  onClickArgs?: any;
+  onClick?: (input: any) => any;
   setOptionsOpen?: (value: boolean) => void;
 };
 
 // A line inside the dropdown menu
-function MenuLine({text, icon, danger = false, onClick = () => {}, setOptionsOpen = () => {}} : MenuLineProps) {
+function MenuLine({text, icon, danger = false, onClickArgs, onClick = () => {}, setOptionsOpen = () => {}} : MenuLineProps) {
   return (
     <div
       className={`flex w-full justify-between px-2 py-1.5 rounded-md cursor-pointer hover:bg-slate-400 hover:bg-opacity-30 ${
         danger ? "mt-2" : ""
       }`}
-      onClick={() => { onClick('group');  setOptionsOpen(false); }}
+      onClick={() => { onClick(onClickArgs);  setOptionsOpen(false); }}
     >
       <p className={danger ? "text-red-500 font-semibold" : ""}>{text}</p>
       <FontAwesomeIcon
@@ -42,72 +49,133 @@ function MenuLine({text, icon, danger = false, onClick = () => {}, setOptionsOpe
 }
 
 type DropdownMenuProps = {
+  chatType: 'dm' | 'group';
   menuOpen: boolean;
-  setInfoOpen: (value: 'user' | 'group' | null) => void;
+  infoData: UserType | GroupType;
+  showInfo: ((group: GroupType) => void) | ((user: UserType) => void);
   setOptionsOpen: (value: boolean) => void;
 };
 
 // The message's dropdown menu
-function DropdownMenu({menuOpen, setInfoOpen, setOptionsOpen} : DropdownMenuProps) {
+function DropdownMenu({chatType, menuOpen, infoData, showInfo, setOptionsOpen} : DropdownMenuProps) {
   return (
     <div
       className={`
         absolute -top-2 right-14 px-2 py-2 w-52 bg-slate-300 rounded-md flex flex-col gap-1 transition-all select-none
-        ${menuOpen ? 'z-10 -top-2' : '-z-10 -top-4'}
+        ${menuOpen ? "z-20 -top-2" : "-z-10 -top-4"}
       `}
     >
-      <MenuLine text="Group info" icon={faCircleInfo} onClick={setInfoOpen} setOptionsOpen={setOptionsOpen} />
-      <MenuLine text="Group settings" icon={faGear} />
-      <MenuLine text="Mute group" icon={faVolumeXmark} />
-      <MenuLine text="Leave group" icon={faArrowRightFromBracket} danger/>
+      {chatType === "group" && (
+        <>
+          <MenuLine
+            text="Group info"
+            icon={faCircleInfo}
+            onClick={showInfo}
+            onClickArgs={infoData}
+            setOptionsOpen={setOptionsOpen}
+          />
+          <MenuLine text="Group settings" icon={faGear} />
+          <MenuLine text="Mute group" icon={faVolumeXmark} />
+          <MenuLine text="Leave group" icon={faArrowRightFromBracket} danger />
+        </>
+      )}
+      {chatType === "dm" && (
+        <>
+          <MenuLine
+            text="Contact info"
+            icon={faCircleInfo}
+            onClick={showInfo}
+            onClickArgs={infoData}
+            setOptionsOpen={setOptionsOpen}
+          />
+          <MenuLine text="Mute group" icon={faVolumeXmark} />
+        </>
+      )}
     </div>
   );
 }
 
-type Props = {
-  setInfoOpen: (value: 'user' | 'group' | null) => void;
-};
+function Chat() {
 
-function Chat({setInfoOpen}: Props) {
 
-  const [messages, setMessages] = useState([
-    {id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}
-  ])
+  // Info panel
+  const showGroupInfo = useInfoStore(state => state.showGroupInfo);
+  const showUserInfo = useInfoStore(state => state.showUserInfo);
+  const closeInfo = useInfoStore(state => state.closeInfo);
+
+  // Fetch Messages for this chat
+  const chats = useChatsStore(state => state.chats);
+  const chatId = useChatsStore(state => state.currentChatId);   
+  const addMessage = useChatsStore(state => state.addMessage);
+  const setInput = useChatsStore(state => state.setInputBuffer);
+  const chat = chats.find((c: ChatType) => c.id === chatId) as GroupType | DMType;
+  const messages = chat.messages;
 
   // Message dropdown
-  const [open, setOpen] = useState<number | null>(null);
+  const [open, setOpen] = useState<string | null>(null);
 
   // Chat dropdown
   const [optionsOpen, setOptionsOpen] = useState(false);
 
-  function handleInput(e: React.FormEvent<HTMLTextAreaElement>) {
-    const newInput = e.currentTarget.value;
+  // Message input
+  // const [input, setInput] = useState('');
+
+  function handleInput(e: React.FormEvent<HTMLTextAreaElement>) {    
+    setInput(chatId, e.currentTarget.value);
   }
 
-  function handleClick(id: number): void {
-    return open === id ? setOpen(null) : setOpen(id);
+  function handleSend() {
+    if (chat.inputBuffer === '') return;
+    addMessage(chatId, chat.inputBuffer);
+    setInput(chatId, '');    
   }
+
+  function handleClick(id: string): void {
+    return open === id ? setOpen(null) : setOpen(id);
+  }  
+
+
+  // Close info panel every time chat changes
+  useEffect(() => {
+    closeInfo();
+    setOpen(null);
+    setOptionsOpen(false);
+  }, [chat])
+  
 
   return (
-    <main className="flex flex-col bg-slate-200 h-screen overflow-x-hidden">
+    <main className="flex flex-col bg-slate-200 h-screen w-full overflow-x-hidden">
       {/* Chat Header */}
       <div className="relative w-full bg-slate-300 pl-5 pr-8 h-20 flex items-center flex-shrink-0">
         {/* Avatar */}
         <div className="bg-slate-900 w-12 h-12 flex flex-shrink-0 items-center justify-center rounded-full select-none">
-          <img className="w-[90%] rounded-full" src={creeper} alt="" />
+          <img className="w-[90%] rounded-full" src={chat.type === 'dm' ? avatar : creeper} alt="" />
         </div>
 
         {/* Name */}
         <div className="flex flex-col pl-3">
-          <h3 className="font-normal text-xl">Pessoal 2.0</h3>
-          <p className="font-normal text-sm">99 participants • 22 online</p>
+          {chat.type === "group" && (
+            <>
+              <h3 className="font-normal text-xl">{chat.name}</h3>
+              <p className="font-normal text-sm">
+                {chat.members.length} participants •{" "}
+                {Math.floor(Math.random() * (chat.members.length + 1))} online
+              </p>
+            </>
+          )}
+          {chat.type === "dm" && (
+            <>
+              <h3 className="font-normal text-xl">{chat.contact.name}</h3>
+              <p className="font-normal text-sm">Online</p>
+            </>
+          )}
         </div>
 
         {/* Ellipsis icon */}
         <div
           className={`
             ml-auto w-12 h-12 flex items-center bg-slate-300 justify-center rounded-xl cursor-pointer hover:brightness-90
-            ${optionsOpen ? 'brightness-90' : ''}
+            ${optionsOpen ? "brightness-90" : ""}
           `}
           onClick={() => setOptionsOpen((prev) => !prev)}
         >
@@ -119,19 +187,27 @@ function Chat({setInfoOpen}: Props) {
         </div>
 
         {/* Dropdown options menu */}
-        <div className='absolute -right-6 top-24 select-none'>
-          <DropdownMenu menuOpen={optionsOpen} setInfoOpen={setInfoOpen} setOptionsOpen={setOptionsOpen} />
+        <div className="absolute -right-6 top-24 select-none">
+          <DropdownMenu
+            chatType={chat.type}
+            menuOpen={optionsOpen}
+            infoData={chat.type === 'dm' ? chat.contact : chat}
+            showInfo={chat.type === "dm" ? showUserInfo : showGroupInfo}
+            setOptionsOpen={setOptionsOpen}
+          />
         </div>
-
       </div>
 
       {/* Messages container */}
-      <div className="px-4 pt-3 pb-4 flex flex-col gap-2 w-full h-full overflow-y-auto">
+      <div className="flex flex-col px-4 pt-4 pb-4 w-full h-full overflow-y-auto">
         {messages.map((m) => (
           <Message
             key={m.id}
             id={m.id}
+            sender={m.sender}
+            content={m.content}
             open={open === m.id}
+            setOpen={setOpen}
             handleClick={handleClick}
           />
         ))}
@@ -143,13 +219,17 @@ function Chat({setInfoOpen}: Props) {
         <TextareaAutosize
           className="bg-slate-200 rounded-md py-3 px-4 flex-grow outline-none resize-none"
           placeholder="Type a message..."
-          maxRows={4}
+          value={chat.inputBuffer}
+          maxRows={10}
           onInput={handleInput}
         />
         {/* Send button */}
-        <div className="bg-slate-200 h-12 w-12 rounded-full relative cursor-pointer">
+        <div
+          className="group bg-slate-200 h-12 w-12 rounded-full relative cursor-pointer hover:bg-slate-400"
+          onClick={handleSend}
+        >
           <FontAwesomeIcon
-            className="text-slate-500 absolute top-[14px] left-[12px]"
+            className="text-slate-500 absolute top-[14px] left-[12px] group-hover:text-slate-100"
             icon={faPaperPlane}
             size="lg"
           />
