853a6ffaf2eb8e764e397fae0117f811e642f55e
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-05 14:05:21 -0300

    Created zustand store for info panel

---
+++ src/zustand/info-panel-store.ts
@@ -0,0 +1,18 @@
+import create from "zustand";
+import { GroupType, UserType } from "../data";
+
+interface State {
+  infoOpen: "user" | "group" | null;
+  data: UserType | GroupType | null;
+  showGroupInfo: (group: GroupType) => void;
+  showUserInfo: (user: UserType) => void;
+  closeInfo: () => void;
+}
+
+export const useInfoStore = create<State>((set) => ({
+  infoOpen: null,
+  data: null,
+  showGroupInfo: (group) => set((state) => ({ ...state, infoOpen: "group", data: group })),
+  showUserInfo: (user) => set((state) => ({ ...state, infoOpen: "user", data: user })),
+  closeInfo: () => set((state) => ({ ...state, infoOpen: null })),
+}));
