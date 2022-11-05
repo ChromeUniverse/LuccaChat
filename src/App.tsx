a752b567861d8e7821631122b89f3c8cda440393
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-05 14:05:51 -0300

    Updated top-level `<App/>` to use info panel store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+// React
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
+// Components
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
+import InfoPanel from "./components/InfoPanel";
+
+// Zustand
+import { useInfoStore } from './zustand/info-panel-store';
+import { GroupType, UserType } from "./data";
 
 function App() {
+
+  const infoOpen = useInfoStore(state => state.infoOpen);
+  const infoData = useInfoStore(state => state.data);
+
   return (
     <div className="flex w-screen h-screen">
       <Sidebar />
-      <div className="h-screen w-1 bg-slate-100"></div>
+      <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
       <Chat />
+
+      {infoOpen !== null && (
+        <>
+          <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
+          {infoOpen === 'user' && <InfoPanel type={infoOpen} user={infoData as UserType} />}
+          {infoOpen === 'group' && <InfoPanel type={infoOpen} group={infoData as GroupType} />}
+        </>
+      )}
     </div>
   );
 }
