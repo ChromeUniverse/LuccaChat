4973689539be9a84d6b687c60df377d8866110c9
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-06 11:51:13 -0300

    Added modals to top-level App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,33 @@ import { useState } from "react";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import InfoPanel from "./components/InfoPanel";
+import AddChat from "./components/modals/AddChat";
 
 // Zustand
 import { useInfoStore } from './zustand/info-panel-store';
 import { GroupType, UserType } from "./data";
+import { useModalStore } from "./zustand/modals-store";
+import AddFriend from "./components/modals/AddFriend";
 
 function App() {
 
   const infoOpen = useInfoStore(state => state.infoOpen);
   const infoData = useInfoStore(state => state.data);
 
+  const modalState = useModalStore(state => state.modalState);
+
   return (
-    <div className="flex w-screen h-screen">
+    <div className="flex w-screen h-screen relative">
+      {/* Modal wrapper */}
+      {modalState && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          {/* Dark translucent background */}
+          <div className="absolute z-10 bg-slate-900 w-full h-full opacity-80"></div>
+          {modalState === 'add-chat' && <AddChat />}
+          {modalState === 'add-friend' && <AddFriend />}
+        </div>
+      )}
+
       <Sidebar />
       <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
       <Chat />
@@ -24,8 +39,12 @@ function App() {
       {infoOpen !== null && (
         <>
           <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
-          {infoOpen === 'user' && <InfoPanel type={infoOpen} user={infoData as UserType} />}
-          {infoOpen === 'group' && <InfoPanel type={infoOpen} group={infoData as GroupType} />}
+          {infoOpen === "user" && (
+            <InfoPanel type={infoOpen} user={infoData as UserType} />
+          )}
+          {infoOpen === "group" && (
+            <InfoPanel type={infoOpen} group={infoData as GroupType} />
+          )}
         </>
       )}
     </div>
