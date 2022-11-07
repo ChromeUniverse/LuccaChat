94a62c80c4dc0e82098303302bfc296c24e2d34c
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-07 15:39:09 -0300

    Updated type definitions

--- src/data.d.ts
+++ src/data.d.ts
@@ -9,23 +9,28 @@ export interface MessageType {
   id: string;
   sender: UserType;
   content: string;
+  createdAt: Date;
 }
 
 export interface ChatType {
-  id: string;  
-  type: "dm" | "group";
+  id: string;
+  // type: "dm" | "group";
   messages: MessageType[];
-  inputBuffer: string;  
+  inputBuffer: string;
 }
 
 export interface GroupType extends ChatType {
-  type: "group",
+  type: "group";
+  group_pfp_url: string;
   name: string;
+  isPublic: boolean;
   description: string;
   members: UserType[];
+  createdBy: UserType;
+  createdAt: Date;
 }
 
 export interface DMType extends ChatType {
-  type: "dm",
-  contact: UserType
+  type: "dm";
+  contact: UserType;
 }
