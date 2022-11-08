db6db68dc34a8ff6848ec61187ab3bb0ec1aafb0
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-07 22:38:07 -0300

    Contact component refactoring

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import avatar from '../assets/avatar.jpeg'
+import { UserType } from "../data";
 import { useChatsStore } from "../zustand/chats-store";
 
 interface Props {
-  name: string;
-  handle: string;
+  user: UserType;
   chatId?: string;
+  highlight?: boolean;
 }
 
-function Contact({ name, handle, chatId}: Props) {
+function Contact({ user, chatId, highlight = false}: Props) {
   
   const currentChatId = useChatsStore(state => state.currentChatId);
   const setChatId = useChatsStore(state => state.setCurrentChatId);
@@ -18,18 +19,19 @@ function Contact({ name, handle, chatId}: Props) {
       className={`
         px-3 py-2 w-full flex items-center gap-3 hover:bg-slate-200 rounded-lg cursor-pointer
         ${chatId === currentChatId ? 'bg-slate-200' : ''}
+        ${highlight ? 'bg-sky-200 hover:bg-sky-200 hover:bg-opacity-50' : ''}
       `}
       onClick={() => {
         if (chatId) setChatId(chatId);
       }}
     >
       {/* Avatar */}
-      <img className="w-14 h-14 rounded-full" src={avatar} alt="" />
+      <img className="w-14 h-14 rounded-full" src={user.pfp_url} alt="" />
 
       {/* Contact Name */}
       <div className="flex flex-col">
-        <h3 className="font-normal text-xl">{name}</h3>
-        <p className="font-bold text-sm">@{handle}</p>
+        <h3 className="font-normal text-xl">{user.name}</h3>
+        <p className="font-bold text-sm">@{user.handle}</p>
       </div>
     </div>
   );
