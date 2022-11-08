e8049c7dd4dd337789bb84223db4ddb87a8283f0
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-07 22:37:24 -0300

    Added basic request click handling functionality

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,34 +1,57 @@
+import React from 'react'
 import { faCheck, faXmark, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { RequestType, useRequestsStore } from '../zustand/requests-store'
 
-type Props = {
-  name: string
+interface ButtonProps {
+  type: 'accept' | 'reject';
+  id: string;
 }
 
 // Request accept/reject button component
-function Button({ type }: {type: 'accept' | 'reject'}) {
+function Button({ type, id }: ButtonProps) {
+  
+  const removeRequest = useRequestsStore(state => state.removeRequest);  
 
-  const icon = type === 'accept' ? faCheck : faXmark;
+  const reject = () => {
+    removeRequest(id);
+  }
+
+  // NOTE: todo!
+  const accept = () => {
+    // ...
+  }
 
   return (
-    <div className="group w-10 h-10 rounded-full bg-slate-300 hover:bg-slate-400 flex items-center justify-center cursor-pointer">
-      <FontAwesomeIcon className='group-hover:text-white' icon={icon}/>      
+    <div
+      className="group w-10 h-10 rounded-full bg-slate-300 hover:bg-slate-400 flex items-center justify-center cursor-pointer"
+      onClick={type === "accept" ? accept : reject}
+    >
+      <FontAwesomeIcon
+        className="group-hover:text-white"
+        icon={type === "accept" ? faCheck : faXmark}
+      />
     </div>
   );
 }
+
+
+interface Props {
+  request: RequestType;
+}
  
-function Request({name}: Props) {
+function Request({ request }: Props) {
+
   return (
     <div className='flex items-center gap-3 w-full'>
       {/* Avatar */}
-      <div className='w-14 h-14 rounded-full bg-slate-400 flex-shrink-0'></div>
+      <img className='w-14 h-14 rounded-full flex-shrink-0' src={request.sender.pfp_url} alt="" />
       {/* Name */}
-      <p className='text-lg'>{name}</p>
+      <p className='text-lg'>{request.sender.name}</p>
       {/* Buttons container */}
       <div className='flex gap-2 ml-auto'>
-        <Button type="reject"/>
-        <Button type="accept"/>
+        <Button type="reject" id={request.id} />
+        <Button type="accept" id={request.id}/>
       </div>
     </div>
   )
