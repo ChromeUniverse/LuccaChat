c3cf138371a13eec9e709dbccb09a16fa41ddb17
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-11 00:49:47 -0300

    Bringing new components into top-level `<App/>`

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,49 @@
 // React
-import { createContext, useContext, useState } from "react";
+import { createContext, useEffect } from "react";
 
 // Components
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import InfoPanel from "./components/InfoPanel";
-import AddChat from "./components/modals/AddChat";
 
 // Zustand
 import { useInfoStore } from "./zustand/info-panel-store";
 import { GroupType, UserType } from "./data";
-import { useModalStore } from "./zustand/modals-store";
-import AddFriend from "./components/modals/AddFriend";
-import CreateGroup from "./components/modals/CreateGroup";
 
 // Auth'd user
-import { user } from "./zustand/chats-store";
+import { useChatsStore, user } from "./zustand/chats-store";
+import ModalWrapper from "./components/modals/ModalWrapper";
+import Home from "./components/Home";
 
 export const AuthContext = createContext(user);
 
 function App() {
   const infoOpen = useInfoStore((state) => state.infoOpen);
   const infoData = useInfoStore((state) => state.data);
+  const currentChatId = useChatsStore((state) => state.currentChatId);
 
-  const modalState = useModalStore((state) => state.modalState);
+
+  useEffect(() => {
+    console.log('App Effect fired!');
+  }, [currentChatId])
 
   return (
     <AuthContext.Provider value={user}>
-      <div className="flex w-screen h-screen relative">
-        {/* Modal wrapper */}
-        {modalState && (
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-            {/* Dark translucent background */}
-            <div className="absolute z-10 bg-slate-900 w-full h-full opacity-80"></div>
-            {modalState === "add-chat" && <AddChat />}
-            {modalState === "add-friend" && <AddFriend />}
-            {modalState === "create-group" && <CreateGroup />}
-          </div>
+      <div className="flex w-screen h-screen relative">        
+        <ModalWrapper />
+        <Sidebar />
+
+        {/* Display Home or Chat views */}
+        {currentChatId === null ? (
+          <Home />
+        ) : (
+          <>
+            <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
+            <Chat />
+          </>
         )}
 
-        <Sidebar />
-        <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
-        <Chat />
-
+        {/* Display info panel */}
         {infoOpen !== null && (
           <>
             <div className="h-screen w-0.5 bg-slate-100 flex-shrink-0"></div>
