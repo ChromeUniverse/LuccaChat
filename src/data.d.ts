67e418735f11bdfa7fa2d0685152245d0104afeb
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-12 09:58:50 -0300

    Updated typedefs

--- src/data.d.ts
+++ src/data.d.ts
@@ -5,6 +5,10 @@ export interface UserType {
   handle: string;
 }
 
+export interface CurrentUserType extends UserType {
+  email: string;
+}
+
 export interface MessageType {
   id: string;
   sender: UserType;
@@ -14,7 +18,7 @@ export interface MessageType {
 
 export interface ChatType {
   id: string;
-  // type: "dm" | "group";
+  unread: number;
   latest: Date;
   messages: MessageType[];
   inputBuffer: string;
