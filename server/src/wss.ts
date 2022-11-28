3949938624c6eae2432ab6277728a512f3df08d1
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-11-27 23:08:32 -0300

    Updated main websockets server

--- server/src/wss.ts
+++ server/src/wss.ts
@@ -14,21 +14,55 @@ import { handleRegenInvite } from "./websockets-handlers/regen-invite";
 import { json } from "stream/consumers";
 import { handleUpdateUserSettings } from "./websockets-handlers/update-user-settings";
 import { handleRemoveMember } from "./websockets-handlers/remove-member";
+import { asyncJWTverify } from "./misc/jwt";
+import { UserJwtReceived } from "../types/jwt";
 
 // Prisma setup
 const prisma = new PrismaClient();
 
 // Websockets server setup
-const wss = new WebSocketServer({ port: 8081 });
+const wss = new WebSocketServer({ port: Number(process.env.WS_PORT) });
 
-const wsUserMap = new Map<WebSocket, string>();
+// const wsUserMap = new Map<WebSocket, string>();
 
-wss.on("connection", function connection(ws) {
-  // for testing purposes only:
-  const userId = "19c5cede-a9ae-4479-81c2-95dc9c0a0e37";
-  wsUserMap.set(ws, userId);
+//
+// TODO: Reverse the Map!!!
+//
+const userSocketMap = new Map<string, WebSocket>();
 
-  console.log(`User ID ${userId} connected`);
+wss.on("listening", () => {
+  console.log(`WebSockets server started on port ${process.env.WS_PORT}`);
+});
+
+wss.on("connection", async function connection(ws, req) {
+  // authenticate incoming websocket connection
+  const cookies = req.headers.cookie;
+  if (!cookies) return ws.close();
+  let currentUser: UserJwtReceived = { id: "", iat: 0 };
+
+  try {
+    // Decode auth JWT
+    const token = cookies.split("=")[1];
+    currentUser = (await asyncJWTverify(
+      token,
+      process.env.JWT_SECRET as string
+    )) as UserJwtReceived;
+  } catch (err) {
+    console.error(err);
+    return ws.close();
+  }
+
+  // check for JWT expiry
+  const expiryTime = Number(process.env.JWT_EXPIRY);
+  if (Math.round(Date.now() / 1000) - currentUser.iat > expiryTime) {
+    return ws.close();
+  }
+
+  // Bind user ID to WebSocket, add it to map
+  ws.userId = currentUser.id;
+  userSocketMap.set(currentUser.id, ws);
+
+  console.log(`User ID ${ws.userId} connected`);
 
   ws.on("message", async function message(rawData) {
     try {
@@ -39,43 +73,43 @@ wss.on("connection", function connection(ws) {
 
       // Process chat messages
       if (data.dataType === "add-message") {
-        handleAddMessage(wsUserMap, prisma, jsonData);
+        handleAddMessage(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "delete-message") {
-        handleDeleteMessage(wsUserMap, prisma, jsonData);
+        handleDeleteMessage(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "send-request") {
-        handleSendRequest(ws, wsUserMap, prisma, jsonData);
+        handleSendRequest(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "remove-request") {
-        handleRemoveRequest(ws, wsUserMap, prisma, jsonData);
+        handleRemoveRequest(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "create-group") {
-        handleCreateGroup(ws, wsUserMap, prisma, jsonData);
+        handleCreateGroup(ws, prisma, jsonData);
       }
 
       if (data.dataType === "update-group") {
-        handleUpdateGroup(ws, wsUserMap, prisma, jsonData);
+        handleUpdateGroup(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "delete-group") {
-        handleDeleteGroup(ws, wsUserMap, prisma, jsonData);
+        handleDeleteGroup(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "regen-invite") {
-        handleRegenInvite(ws, wsUserMap, prisma, jsonData);
+        handleRegenInvite(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "update-user-settings") {
-        handleUpdateUserSettings(ws, wsUserMap, prisma, jsonData);
+        handleUpdateUserSettings(ws, userSocketMap, prisma, jsonData);
       }
 
       if (data.dataType === "remove-member") {
-        handleRemoveMember(ws, wsUserMap, prisma, jsonData);
+        handleRemoveMember(ws, userSocketMap, prisma, jsonData);
       }
     } catch (error) {
       console.error(error);
@@ -83,8 +117,8 @@ wss.on("connection", function connection(ws) {
   });
 
   ws.on("close", function () {
-    const userId = wsUserMap.get(ws);
-    console.log(`User ID ${userId} has disconnected`);
-    wsUserMap.delete(ws);
+    if (!ws.userId) return;
+    console.log(`User ID ${ws.userId} has disconnected`);
+    userSocketMap.delete(ws.userId);
   });
 });
