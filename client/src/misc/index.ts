65f38937c6bc4682021b27f9c673c080a3007863
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-12-02 17:22:43 -0300

    Updated miscellaneous funcions

--- client/src/misc/index.ts
+++ client/src/misc/index.ts
@@ -3,6 +3,8 @@
 // https://stackoverflow.com/a/1793474 (correct time format)
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
+import { DMType, GroupType } from "../data";
+
 const dateFormat =
   /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(Z|([+\-])(\d{2}):(\d{2}))$/;
 
@@ -16,3 +18,17 @@ function reviver(key: any, value: any) {
 export function JsonSuperParse(stringifiedJSON: string) {
   return JSON.parse(stringifiedJSON, reviver);
 }
+
+export const copyInviteLinkToClipboard = async (chat: GroupType) => {
+  const inviteLink = `${import.meta.env.VITE_REACT_APP_URL}/invite/${
+    chat.inviteCode
+  }`;
+  console.log(inviteLink);
+  await navigator.clipboard.writeText(inviteLink);
+  console.log("copied!");
+};
+
+export const copyMsgContentToClipboard = async (content: string) => {
+  await navigator.clipboard.writeText(content);
+  console.log("copied!");
+};
