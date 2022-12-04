130b17a986239d63ff7d7900abfb5ac6764a4905
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2022-12-04 00:08:16 -0300

    Updated client data fetching functions

--- client/src/api/fetchCurrentUser.ts
+++ client/src/api/fetchCurrentUser.ts
@@ -1,15 +1,15 @@
 import axios, { AxiosError } from "axios";
-import { userSchema } from "../../../server/src/zod/api-chats";
+import { userSchema } from "../../../server/src/zod/user";
 
 export default async function fetchCurrentUser() {
   try {
     // GET request
     const url = `${import.meta.env.VITE_BACKEND_URL}/api/user`;
     const { data, status } = await axios.get(url, { withCredentials: true });
-    return userSchema.parse(data);
+    return { data: userSchema.parse(data), status };
   } catch (err) {
     const error = err as AxiosError;
     console.error(err);
-    return { id: null, name: null, handle: null };
+    return { data: null, status: error.status || -1 };
   }
 }
--- client/src/api/fetchRequests.ts
+++ client/src/api/fetchRequests.ts
@@ -4,12 +4,16 @@ import {
   requestArraySchemaPrimitive,
   requestSchema,
 } from "../../../server/src/zod/api-requests";
+import { JsonSuperParse } from "../misc";
 
 export default async function fetchRequests() {
   try {
     // GET request
     const url = `${import.meta.env.VITE_BACKEND_URL}/api/requests`;
-    const { data, status } = await axios.get(url, { withCredentials: true });
+    const { data, status } = await axios.get(url, {
+      withCredentials: true,
+      transformResponse: (res) => JsonSuperParse(res),
+    });
     return { data: requestArraySchema.parse(data), status };
   } catch (err) {
     const error = err as AxiosError;
